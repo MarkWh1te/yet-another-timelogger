b3518db9b85ee45a547bc2556e3700f7aab695c6
markwh1te <iamwh1temark@outlook.com>
2019-02-18 19:55:52 +0800

    update url

--- yet-another-timelogger/record/urls.py
+++ yet-another-timelogger/record/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include, re_path
 from record import views as v
 
 urlpatterns = [
-    path('record', v.record, name="record"),
+    path('', v.record, name="record"),
     re_path(r'^delete/(?P<part_id>[0-9]+)/$', v.delete, name='delete_view'),
 ]
